5e9499d10f55c4d811413f030fc5835b87232cc0
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-08-29 18:22:31 -0300

    Initial commit

---
+++ src/main/java/futurodev/acoes_futuro_api/AcoesFuturoApiApplication.java
@@ -0,0 +1,13 @@
+package futurodev.acoes_futuro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcoesFuturoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcoesFuturoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/futurodev/acoes_futuro_api/AcoesFuturoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package futurodev.acoes_futuro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcoesFuturoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
