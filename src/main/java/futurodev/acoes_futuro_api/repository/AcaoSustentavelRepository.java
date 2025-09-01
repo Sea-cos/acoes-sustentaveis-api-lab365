d8ae23115ba964deec3fb16387d20b6d2bc7ca76
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-09-01 19:55:08 -0300

    Feat: Pesquisa por categoria -Criado metodo para pesquisar por categoria -Validado retorno vazio -Validado categoria invalida.

--- src/main/java/futurodev/acoes_futuro_api/repository/AcaoSustentavelRepository.java
+++ src/main/java/futurodev/acoes_futuro_api/repository/AcaoSustentavelRepository.java
@@ -1,9 +1,13 @@
 package futurodev.acoes_futuro_api.repository;
 
+import futurodev.acoes_futuro_api.model.Enums.CategoriaAcao;
 import futurodev.acoes_futuro_api.model.entity.AcaoSustentavel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AcaoSustentavelRepository extends JpaRepository<AcaoSustentavel, Long> {
+    List<AcaoSustentavel> findByCategoria(CategoriaAcao categoria);
 }
